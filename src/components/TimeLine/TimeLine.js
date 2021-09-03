47b9a0b6e86ef64d9189f74592eb8b3ab3f164f5
piyush <iampiyushkr@gmail.com>
2021-09-03 17:34:58 +0530

    components

---
+++ src/components/TimeLine/TimeLine.js
@@ -0,0 +1,88 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+import { CarouselButton, CarouselButtonDot, CarouselButtons, CarouselContainer, CarouselItem, CarouselItemImg, CarouselItemText, CarouselItemTitle, CarouselMobileScrollNode } from './TimeLineStyles';
+import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { TimeLineData } from '../../constants/constants';
+
+const TOTAL_CAROUSEL_COUNT = TimeLineData.length;
+
+const Timeline = () => {
+  const [activeItem, setActiveItem] = useState(0);
+  const carouselRef = useRef();
+
+  const scroll = (node, left) => {
+    return node.scrollTo({ left, behavior: 'smooth' });
+  }
+
+  const handleClick = (e, i) => {
+    e.preventDefault();
+
+    if (carouselRef.current) {
+      const scrollLeft = Math.floor(carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length));
+      
+      scroll(carouselRef.current, scrollLeft);
+    }
+  }
+
+  const handleScroll = () => {
+    if (carouselRef.current) {
+      const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * TimeLineData.length);
+
+      setActiveItem(index);
+    }
+  }
+
+  
+  useEffect(() => {
+    const handleResize = () => {
+      scroll(carouselRef.current, 0);
+    }
+
+    window.addEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <Section id = "about">
+      <br /><br />
+      <SectionDivider />
+      <br />
+      <SectionTitle>About Me</SectionTitle>
+      <SectionText>
+        Recent computer science graduate with a passion of developing scalable web application and working across the full stack. I have designed the clone of some of the websites using React, Node.js and Mongodb. Curious to learn and apply my skills on a large scale.
+      </SectionText>
+      <CarouselContainer ref = {carouselRef} onScroll = {handleScroll}>
+        <>
+          {TimeLineData.map((item, index) => {
+            <CarouselMobileScrollNode key = {index} final = {index === TOTAL_CAROUSEL_COUNT - 1}>
+              <CarouselItem
+                index = {index}
+                id={`carousel__item-${index}`}
+                active = {activeItem}
+                onClick = {(e) => handleClick(e, index)}
+              >
+                <CarouselItemTitle>
+                  {item.year}
+                </CarouselItemTitle>
+                <CarouselItemText>{item.text}</CarouselItemText>
+              </CarouselItem>
+            </CarouselMobileScrollNode>
+          })}
+        </>
+      </CarouselContainer>
+      <CarouselButtons>
+        {TimeLineData.map((item, index) => (
+          <CarouselButton
+            key = {index}
+            index = {index}
+            active = {activeItem}
+            onClick = {(e) => handleClick(e, index)}
+          >
+            <CarouselButtonDot active = {activeItem} />
+          </CarouselButton>
+        ))}
+      </CarouselButtons>
+    </Section>
+  );
+};
+
+export default Timeline;

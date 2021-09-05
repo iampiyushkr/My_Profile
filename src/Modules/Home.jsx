7a4d3f204dbcf151b4e09f93bd69459051675c95
piyush <iampiyushkr@gmail.com>
2021-09-05 22:51:29 +0530

    new

---
+++ src/Modules/Home.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import CustomNav from "../Components/NavBar";
+import { Container, Button, Image } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import CodingSvg from "../Icons/coding";
+import useWindowDimensions from "../utils/dimensionHelper";
+
+// library.add()
+
+const Home = ({ history }) => {
+  const { width } = useWindowDimensions();
+
+  return (
+    <div>
+      <CustomNav />
+      <div
+        className="position-absolute"
+        style={{
+          height: "100vh",
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          zIndex: -1,
+        }}
+      >
+        <Container className="h-100">
+          <div className="d-flex h-100">
+            <div className={`my-auto ${width > 760 ? "w-50" : ""}`}>
+              <div>
+                <div className="">
+                  <span className="color-customBlueLight h5 font-weight-bold">
+                    Hello !
+                  </span>
+                  <br />
+                  <span className="color-customBlue h1 font-weight-bold">
+                    I'm Piyush Kumar
+                  </span>
+                  <br />
+                  <span className="color-customBlueLight h3">
+                    I'm a <span className="font-weight-bold">Full Stack</span>{" "}
+                    Developer
+                  </span>
+                  <br />
+                  <br />
+                  <span className="color-customBlueLight h5">
+                    I enjoy developing websites and web applications.
+                  </span>
+                  <br />
+                </div>
+                <div>
+                  <div className="d-flex mt-4">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://github.com/iampiyushkr"
+                      className="color-customBlue p-0 mr-3"
+                    >
+                      <FontAwesomeIcon size="2x" icon={faGithubSquare} />
+                    </a>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://www.linkedin.com/in/piyush-kumar-09/"
+                      className="color-customBlue p-0 mr-3"
+                    >
+                      <FontAwesomeIcon size="2x" icon={faLinkedin} />
+                    </a>
+                  </div>
+                  <br />
+                  <Button
+                    className="bg-customBlue font-weight-bold shadow border-0"
+                    onClick={() => window.open("/resume.pdf", "_blank")}
+                  >
+                    Resume
+                  </Button>
+                </div>
+              </div>
+            </div>
+            <div className="w-50 my-auto d-none d-md-block ">
+              {/* <CodingSvg /> */}
+              <Image
+                src="images/piyush.jpg"
+                thumbnail
+                fluid
+                className="profilePic"
+              />
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/utils/helperList.js
@@ -0,0 +1,129 @@
+export const showcaseSkills = [
+  {
+    label: "Javascript",
+    rating: 81,
+  },
+  {
+    label: "Node JS",
+    rating: 81,
+  },
+  {
+    label: "React",
+    rating: 81,
+  },
+  {
+    label: "Mongo dB",
+    rating: 72,
+  },
+  {
+    label: "Redux",
+    rating: 73,
+  },
+  {
+    label: "Express",
+    rating: 74,
+  },
+  {
+    label: "HTML CSS",
+    rating: 85,
+  },
+];
+
+export const frontSkillsList = [
+  {
+    label: "HTML5",
+    src: "https://img.icons8.com/color/480/000000/html-5--v1.png",
+  },
+  {
+    label: "CSS3",
+    src: "https://img.icons8.com/color/480/000000/css3.png",
+  },
+  {
+    label: "Javascript",
+    src: "https://img.icons8.com/color/480/000000/javascript.png",
+  },
+  {
+    label: "React",
+    src: "https://img.icons8.com/color/480/000000/react-native.png",
+  },
+  {
+    label: "Redux",
+    src: "https://img.icons8.com/color/480/000000/redux.png",
+  },
+  {
+    label: "Material UI",
+    src: "https://material-ui.com/static/logo_raw.svg",
+  },
+  {
+    label: "Bootstrap",
+    src: "https://i.ibb.co/zx0Tt9M/bootstrap.png",
+  },
+  {
+    label: "Github",
+    src: "https://img.icons8.com/fluent/480/000000/github.png",
+  },
+  {
+    label: "NPM",
+    src: "https://img.icons8.com/color/480/000000/npm.png",
+  },
+ 
+];
+
+export const backSkillsList = [
+  {
+    label: "Node JS",
+    src: "https://img.icons8.com/color/480/000000/nodejs.png",
+  },
+  {
+    label: "Express JS",
+    src: "https://i.ibb.co/9smDM6j/pngegg.png",
+  },
+  {
+    label: "Mongo dB",
+    src: "https://i.ibb.co/ZVh01dm/rsz-mong2.png",
+  },
+ 
+];
+
+export const projectsList = [
+  {
+    label: "Coursera Clone",
+    imgSrc: "images/coursera.png",
+    src: "#",
+    githubSrc: "https://github.com/vedascript/coursera-clone/tree/master",
+    description: "An EdTech Platform with all MVP features",
+    stack: ["React", "Redux", "Express", "Mongoose", "Node.js", "Material-UI"],
+  },
+  {
+    label: "Blog Platform",
+    imgSrc: "images/blog-site.png",
+    src: "",
+    githubSrc: "https://github.com/vedascript/blog-site",
+    description: "A Blog platform where user can publish,edit,delete his blogs",
+    stack: ["React", "Redux", "Express", "Mongoose", "Node.js", "CSS"],
+  },
+  {
+    label: "Expense Tracker",
+    imgSrc: "images/expenses.png",
+    src: "https://expenssetrackerr.netlify.app/",
+    githubSrc: "https://github.com/vedascript/expenseTracker",
+    description: "An expense tracker to track user's expenses",
+    stack: ["React", "React-Charts", "Material-UI"],
+  },
+  {
+    label: "Sudoku Solver",
+    imgSrc: "images/sudoku.png",
+    src: "https://sudokusolvervw.netlify.app",
+    githubSrc: "https://github.com/vedascript/sudokuSolver",
+    description: "A automated sudoku solver - an aaplication of backtraking",
+    stack: ["JavaScript", "HTML", "CSS"],
+  },
+  {
+    label: "Fathom Health Clone",
+    imgSrc: "images/fathom.png",
+    src: "https://fathomhealth.netlify.app",
+    githubSrc: "https://github.com/vedascript/fathomhealth-clone",
+    description: "A visual clone of Fathom Health completed in 3 days",
+    stack: ["JavaScript", "HTML", "CSS"],
+  },
+];
